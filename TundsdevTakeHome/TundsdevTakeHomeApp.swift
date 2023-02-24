7caafa8e896618fafea118a22f8f96391d3e43c1
IsmailUlasUnal <ismailulasunal@gmail.com>
2023-02-24 13:49:20 +0300

    Initial Commit

---
+++ TundsdevTakeHome/TundsdevTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TundsdevTakeHomeApp.swift
+//  TundsdevTakeHome
+//
+//  Created by Ulaş Ünal on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TundsdevTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
